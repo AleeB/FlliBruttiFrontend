736015ea0616152b2dd5257901a9e3818d524842
Alessandro Polenta <alessandro.polenta@studenti.unicam.it>
2026-01-02 01:45:54 +0100

    Versione 1, aggiunte pagine e check interfaccia, manca la pagina per il movimento terra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@
 export const routes: Routes = [
 	{ path: '', loadComponent: () => import('./pages/home/index.component').then(m => m.IndexComponent) },
 	{ path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
-	{ path: 'ncc', title: 'NCC', loadComponent: () => import('./pages/home/index.component').then(m => m.IndexComponent) },
-	{ path: 'movimento-terra', title: 'Movimento Terra', loadComponent: () => import('./pages/home/index.component').then(m => m.IndexComponent) },
+	{ path: 'ncc', title: 'NCC', loadComponent: () => import('./pages/preventivi/ncc/ncc.component').then(m => m.NccComponent) },
+	// { path: 'movimento-terra', title: 'Movimento Terra', loadComponent: () => import('./pages/preventivi/movimento-terra/movimento-terra.component').then(m => m.MovimentoTerraComponent) },
 	{ path: 'preventivi', title: 'Preventivi', loadComponent: () => import('./pages/preventivi/preventivi.component').then(m => m.PreventiviComponent) }
 ];
 	
---
+++ src/app/pages/preventivi/ncc/ncc.component.ts
@@ -0,0 +1,37 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+
+@Component({
+  selector: 'app-ncc',
+  standalone: true,
+  imports: [CommonModule, RouterModule, ButtonModule, TableModule, FormsModule, ReactiveFormsModule, InputNumberModule, InputTextModule, CheckboxModule],
+  templateUrl: './ncc.component.html',
+  styleUrls: ['./ncc.component.scss']
+})
+export class NccComponent {
+  nccForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.nccForm = this.fb.group({
+      cittaPartenza: ['', Validators.required],
+      cittaArrivo: ['', Validators.required],
+      giorniPermanenza: [1, [Validators.required, Validators.min(1)]],
+      numeroPersone: [1, [Validators.required, Validators.min(1), Validators.max(8)]],
+      ztl: [false]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.nccForm.valid) {
+      console.log('nccForm', this.nccForm.value);
+    }
+  }
+}
