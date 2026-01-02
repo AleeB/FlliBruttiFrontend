dbd4ddb6995222d71f2fb35e95bc617011608ee1
Alessandro Polenta <alessandro.polenta@studenti.unicam.it>
2026-01-02 11:59:45 +0100

    Aggiunta la pages movimento terra, da finire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
 	{ path: '', loadComponent: () => import('./pages/home/index.component').then(m => m.IndexComponent) },
 	{ path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
 	{ path: 'ncc', title: 'NCC', loadComponent: () => import('./pages/preventivi/ncc/ncc.component').then(m => m.NccComponent) },
-	// { path: 'movimento-terra', title: 'Movimento Terra', loadComponent: () => import('./pages/preventivi/movimento-terra/movimento-terra.component').then(m => m.MovimentoTerraComponent) },
+	{ path: 'movimento-terra', title: 'Movimento Terra', loadComponent: () => import('./pages/preventivi/movimentoTerra/movimentoTerra.component').then(m => m.MovimentoTerraComponent) },
 	{ path: 'preventivi', title: 'Preventivi', loadComponent: () => import('./pages/preventivi/preventivi.component').then(m => m.PreventiviComponent) }
 ];
 	
